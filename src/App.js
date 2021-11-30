fde8e4c092d221737909d852b55f631af8c3dc0f
francocava <francocavallini@hotmail.com>
2021-11-30 18:31:10 -0300

    Mejoras

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState, useEffect } from "react";
+import Header from "./components/Header";
+import Forumulario from "./components/Formulario";
+import Clima from "./Clima";
 
 function App() {
+
+  const [busqueda, guardarBusqueda] = useState({
+    ciudad: '',
+    pais: ''
+  });
+
+  const [consultar, guardarConsultar] = useState(false);
+  //guarda un objeto (lo que devuelve la api)
+  const [resultado, guardarResultado] = useState({});
+  const {ciudad, pais} = busqueda;
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      
+      if(consultar){
+        
+        const appId = 'f2dd9bc2c7c91acc3db0bcb8db6bb9e6';
+        const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+
+        guardarResultado(resultado);
+        guardarConsultar(false);
+      }
+    }
+    consultarAPI();
+  }, [consultar]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Header titulo="Clima" />
+
+      <div className="contenedor-form">
+        <div className="coontainer">
+          <div className="row">
+            <div className="col m6 s12">
+              <Forumulario 
+              busqueda={busqueda}
+              guardarBusqueda={guardarBusqueda}
+              guardarConsultar={guardarConsultar}/>
+            </div>
+            <div className="col m6 s12">
+              <Clima
+              resultado={resultado}/>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </Fragment>
   );
 }
 
---
+++ src/components/Formulario.js
@@ -0,0 +1,73 @@
+import React, {useState} from "react";
+
+const Forumulario = ({busqueda,guardarBusqueda, guardarConsultar}) => {
+
+
+  
+  const {ciudad, pais} = busqueda;
+  const [error, guardarError] = useState(false);
+
+  const handleChange = e => {
+      guardarBusqueda({
+          ...busqueda,
+          [e.target.name] : e.target.value
+      });
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    if(ciudad.trim() ==='' || pais.trim() === '' ) {
+      guardarError(true);
+      return;
+    }
+
+    guardarError(false);
+
+    guardarConsultar(true);
+
+  }
+
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+    >
+      {error ? <p className="red darken-4 error">Completar</p> : null}
+      <div className="input-field col s12">
+        <input type="text" 
+            name="ciudad" 
+            id="ciudad" 
+            value={ciudad}
+            onChange={handleChange} 
+            />
+        <label htmlFor="ciudad">Ciudad: </label>
+      </div>
+
+      <div className="input-field col s12">
+        <select 
+            name="pais" 
+            id="pais" 
+            value={pais}
+            onChange={handleChange}
+            >
+          <option value="">Elegir</option>
+          <option value="US">Estados Unidos</option>
+          <option value="MX">México</option>
+          <option value="AR">Argentina</option>
+          <option value="CO">Colombia</option>
+          <option value="CR">Costa Rica</option>
+          <option value="ES">España</option>
+          <option value="PE">Perú</option>
+        </select>
+        <label htmlFor="pais">Pais: </label>
+      </div>
+      <div className="input-field col s12">
+        <input type="submit" value="Buscar Clima" className="waves-effect waves-light btn-large btn-block yellow accent-4">
+        </input>
+      </div>
+    </form>
+  );
+};
+
+export default Forumulario;
